b4375e7cb0d431ad9789a35ef2fb1d99e13511d0
Meshhur <meshhuratageldiyev010@gmail.com>
2022-10-30 22:33:33 +0300

    api key changed to public

--- src/api/getWeatherData.js
+++ src/api/getWeatherData.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getWeatherData = async (lat, lon) => {
     const { data } = await axios({
         method: "get",
-        url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_API_KEY}`,
+        url: `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=fbebef5fdf41f22567e1cb9c029fbdaf`,
     });
 
     return data;
